5d83fe58467298251075a93554e619f255cd7ce1
Josh Hurt <josh.hurt@provisionsgroup.com>
2023-01-18 09:26:17 -0600

    PROD-61 Add Google Tag Manager

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TagManager from 'react-gtm-module';
+import App from './App';
+import './index.css';
+
+// Available args: https://github.com/alinemorelli/react-gtm
+const gtmStagingId = 'GTM-MKG6L3V';
+const tagManagerArgs = {
+  gtmId: gtmStagingId,
+  dataLayerName: 'dataLayer',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
